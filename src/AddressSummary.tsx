1bc1bf67683f6b6025cddc0b088824413065c151
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-01-15 16:09:57 -0600

    Fix int'l address summary

--- src/AddressSummary.tsx
+++ src/AddressSummary.tsx
@@ -1,3 +1,4 @@
+import { compact } from "lodash";
 import { Address } from "./types";
 
 interface Props {
@@ -5,12 +6,18 @@ interface Props {
 }
 
 export function AddressSummary({ address }: Props) {
+  const localityLine = compact([
+    address.locality,
+    address.administrativeAreaCode,
+    address.postalCode,
+  ]).join(', ')
+
   return (
     <address>
       {address.contactName}<br />
       {address.streetAddress}<br />
       {address.extendedAddress}
-      {`${address.locality}, ${address.administrativeAreaCode}, ${address.postalCode}`}<br />
+      {localityLine}<br />
       {address.countryCode}
     </address>
   );
